644c50c7857b7c3ffa5d316f56cae13f0902c0b3
Bert Hertogen <bert@hertogen.net>
2023-07-15 14:11:33 +0200

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        'src/site.webmanifest',
+        'src/browserconfig.xml',
+        'src/sitemap.xml',
+        'src/robots.txt',
+        { from: 'src/assets/icons', to: 'assets/icons' },
+        { from: 'src/assets/download', to: 'assets/download' },
+        { from: 'src/assets', to: 'assets' },
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css"
+    })
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  ignoreWarnings: [
+    {
+      module: /bootstrap.scss/,
+      message: /deprecated/,
+    },
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          'html-loader'
+        ]
+      },
+    ],
+  },
+};
