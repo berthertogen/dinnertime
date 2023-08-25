5dea8b6a332daa017e9e0be6c727e9be85cf4883
Bert Hertogen <bert@hertogen.net>
2023-08-25 10:10:29 +0200

    Add test time

--- src/routes/Nap.svelte
+++ src/routes/Nap.svelte
@@ -3,11 +3,28 @@
 	import IconButton from '@smui/icon-button';
 	import { last3Naps, lastNap, napsToday, lastNapElapsed, timeFromLastNap } from './stores';
 	import { createEventDispatcher } from 'svelte';
+	import Dialog, { Actions } from '@smui/dialog';
+	import Button, { Label } from '@smui/button';
+	import { invalidateAll } from '$app/navigation';
+	import Textfield from '@smui/textfield';
+
+	$: open = false;
+	let timeFrom = '';
+	let timeTo = '';
 
 	const dispatch = createEventDispatcher();
 
-	function deleteNap(dinner: any) {
-		dispatch('deleteNap', dinner);
+	function deleteNap(nap: any) {
+		dispatch('deleteNap', nap);
+	}
+	async function openNapUpdater(nap: any) { 
+		open = true;
+	}
+
+	async function updateNap() {
+		invalidateAll().then(() => {
+			open = false;
+		});
 	}
 </script>
 
@@ -25,11 +42,32 @@
 		{#each $last3Naps as nap}
 			<div class="row">
 				{nap.from} - {nap.till} ({nap.elapsed || $lastNapElapsed} hours)
+				<IconButton class="material-icons" on:click={() => openNapUpdater(nap)}>edit</IconButton>
 				<IconButton class="material-icons" on:click={() => deleteNap(nap)}>delete</IconButton>
 			</div>
 		{/each}
 	</Content>
 </Paper>
+
+<Dialog bind:open aria-labelledby="simple-title" aria-describedby="simple-content">
+	<Textfield type="time" bind:value={timeFrom} label="From"/>
+	<Textfield type="time" bind:value={timeTo} label="To"/>
+	<pre>
+		{timeFrom} - {timeTo}
+	</pre>
+	<Actions>
+		<Button on:click={() => (open = false)}>
+			<Label>Cancel</Label>
+		</Button>
+		<Button
+			on:click={async () => {
+				await updateNap();
+			}}
+		>
+			<Label>Save</Label>
+		</Button>
+	</Actions>
+</Dialog>
 
 <style>
 	.row:last-child {
